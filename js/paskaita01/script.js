7aa671c79b201d74d124e641b3ef63bae32cef8d
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-11-12 19:41:52 +0200

    paskaita 01

---
+++ js/paskaita01/script.js
@@ -0,0 +1,53 @@
+console.log("Labas");
+console.log("Dar kazkas");
+
+const vardas = "Marius";
+const pavarde = 'kajsdhflasdfjk laksdjbf@;alsdgf'
+const kazkas = `askl;djfl;asdfkj`
+
+let amzius = 50;
+const amzius2 = "50"
+
+amzius = 51;
+
+console.log(vardas);
+console.log(amzius);
+
+amzius = 60;
+
+const skaicius1 = 20;
+
+const suma = amzius + skaicius1 + 25;
+const atimtis = 20 - 5;
+const sandauga = 15 * 2;
+const dalmuo = 15 / (3 * 5) - 10;
+
+console.log(suma);
+console.log(amzius);
+
+
+
+
+// alert("Laba diena")
+
+// const vartotojoIvestasDalykas = prompt("Iveskite cia kazka");
+
+// let nieko = null;
+// let nieko2 = undefined;
+// console.log(nieko);
+// console.log(vartotojoIvestasDalykas);
+
+// const birthday = "gegužės 12";
+// const name = prompt("Koks tavo vardas?");
+
+// const rez = name + " yra gimęs " + birthday;
+
+// console.log(rez);
+
+const lietuvosBiudzetas = 8487300000;
+const darbingiZmones = 1761463;
+const bazinisUzmokestis = prompt('Iveskite bazini uzmokesti');
+
+const rez = lietuvosBiudzetas / (darbingiZmones * bazinisUzmokestis) * 100;
+console.log(rez);
+
